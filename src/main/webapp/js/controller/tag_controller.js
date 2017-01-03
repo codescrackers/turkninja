2decf2dbda4ce1d1cc6a5e2dfb1ada4af2b70a53
Kadir Yaka <yakakadir@gmail.com>
2017-01-03 22:06:51 +0200

    anasayfa blog'a angular eklendi
    Change-Id: Id659fdda1f0380069f03fc63f0602d49587023fd

---
+++ src/main/webapp/js/controller/tag_controller.js
@@ -0,0 +1,9 @@
+'use strict';
+ 
+angular.module('blog').controller('TagController', ['$scope', 'TagService', function($scope, TagService) {
+	
+	var vm = this;
+	
+	vm.deneme = "Java";
+	
+}]);
---
+++ src/main/webapp/js/service/tag_service.js
@@ -0,0 +1,11 @@
+'use strict';
+ 
+angular.module('blog').factory('TagService', ['$http', '$q', function($http, $q){
+ 
+    var factory = {
+            
+        };
+     
+        return factory;
+	
+}]);
